e51482f5d3ee57e81827511a3c75f5d412ad5681
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-08-11 18:07:47 +0100

    differentiate more the color scale for the histogram

--- pyterrier_colbert/ranking.py
+++ pyterrier_colbert/ranking.py
@@ -601,7 +601,8 @@ class ColBERTFactory():
             plt.text(i-0.25, maxpos+0.1, "X", fontsize=5)
             contributions.append(interaction[maxpos,i])
 
-        ax1.bar([0.5 + i for i in range(0,32)], contributions, color=plt.cm.Blues(contributions))
+        from sklearn.preprocessing import minmax_scale
+        ax1.bar([0.5 + i for i in range(0,32)], contributions, color=plt.cm.Blues(minmax_scale(contributions)))
         ax1.set_xlim([0,32])
         ax1.set_xticklabels([])
         fig.tight_layout()
