a0057cd49b857661070a98c25d6d8a721d1fe9c9
Hammad Khan <hammadkhann34@gmail.com>
2021-10-16 21:51:21 +0100

    added query_encode=True support in text_scorer method.

--- pyterrier_colbert/ranking.py
+++ pyterrier_colbert/ranking.py
@@ -441,6 +441,30 @@ class ColBERTFactory():
         
         return pt.apply.generic(_single_retrieve_qembs if query_encoded else _single_retrieve)
 
+    def slow_rerank_with_qembs(args, qembs, pids, passages, gpu=True):
+        colbert = args.colbert
+        inference = args.inference
+
+        # make to 3d tensor
+        Q = torch.unsqueeze(qembs, 0)
+        if gpu:
+            Q = Q.cuda()
+        
+        D_ = inference.docFromText(passages, bsize=args.bsize)
+        if gpu:
+            D_ = D_.cuda()
+        
+        scores = (Q @ D_.permute(0, 2, 1)).max(2).values.sum(1)
+
+        scores = scores.sort(descending=True)
+        ranked = scores.indices.tolist()
+
+        ranked_scores = scores.values.tolist()
+        ranked_pids = [pids[position] for position in ranked]
+        ranked_passages = [passages[position] for position in ranked]
+
+        return list(zip(ranked_scores, ranked_pids, ranked_passages))
+
     def text_scorer(self, query_encoded=False, doc_attr="text", verbose=False) -> TransformerBase:
         """
         Returns a transformer that uses ColBERT model to score the *text* of documents.
@@ -451,7 +475,6 @@ class ColBERTFactory():
 
         #output: qid, query, docno, score
 
-        assert not query_encoded
         def _text_scorer(queries_and_docs):
             groupby = queries_and_docs.groupby("qid")
             rtr=[]
@@ -463,7 +486,20 @@ class ColBERTFactory():
                             rtr.append([qid, query, pid, score, rank])          
             return pd.DataFrame(rtr, columns=["qid", "query", "docno", "score", "rank"])
 
-        return pt.apply.generic(_text_scorer)
+        # when query is encoded 
+        def _text_scorer_qembs(queries_and_docs):
+            groupby = queries_and_docs.groupby("qid")
+            rtr=[]
+            with torch.no_grad():
+                for qid, group in tqdm(groupby, total=len(groupby), unit="q") if verbose else groupby:
+                    qembs = group["query_embs"].values[0]
+                    query = group["query"].values[0]
+                    ranking = slow_rerank_with_qembs(self.args, qembs, group["docno"].values, group[doc_attr].values.tolist())
+                    for rank, (score, pid, passage) in enumerate(ranking):
+                            rtr.append([qid, query, pid, score, rank])          
+            return pd.DataFrame(rtr, columns=["qid", "query", "docno", "score", "rank"])
+
+        return pt.apply.generic(_text_scorer_qembs if query_encoded else _text_scorer)
 
     def _add_docids(self, df):
         if self.docid_as_docno:
