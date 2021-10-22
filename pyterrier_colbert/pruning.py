11957d0a1596f25a4f40dc1975c2973f699b8e3a
thorongil <officialantonio@outlook.it>
2021-10-22 18:39:50 +0100

    Add new transformers
    *add fetch_index_encodings
    *add pca_transformer
    *add scorer
    *add blacklisted_tokens_transformer

--- pyterrier_colbert/pruning.py
+++ pyterrier_colbert/pruning.py
@@ -131,3 +131,124 @@ def query_embedding_pruning_special(CLS=False, Q=False, MASK=False) -> Transform
         row["query_embs"] = row["query_embs"][final_mask]
         return row
     return pt.apply.generic(lambda df : df.apply(row_rewriter, axis=1))
+
+def fetch_index_encodings(factory, verbose=False, ids=False) -> TransformerBase:
+    """
+    New encoder that gets embeddings from rrm and stores into doc_embs column.
+    If ids is True, then an additional doc_toks column is also added. This requires 
+    a Faiss NN term index data structure, i.e. indexing should have ids=True set.
+    input: docid, ...
+    output: ditto + doc_embs [+ doc_toks]
+    """
+    def _get_embs(df):
+        if verbose:
+            import pyterrier as pt
+            pt.tqdm.pandas()
+            df["doc_embs"] = df.docid.progress_apply(factory.rrm.get_embedding)
+        else:
+            df["doc_embs"] = df.docid.apply(factory.rrm.get_embedding)
+        return df
+
+    def _get_tok_ids(df):
+        fnt = factory.nn_term(False)
+        def _get_toks(pid):
+            end = fnt.end_offsets[pid]
+            start = end - fnt.doclens[pid]
+            return fnt.emb2tid[start:end]
+
+        if verbose:
+            import pyterrier as pt
+            pt.tqdm.pandas()
+            df["doc_toks"] = df.docid.progress_apply(_get_toks)
+        else:
+            df["doc_toks"] = df.docid.apply(_get_toks)
+        return df
+    rtr = pt.apply.by_query(_get_embs, add_ranks=False)
+    if ids:
+        rtr = rtr >> pt.apply.by_query(_get_tok_ids, add_ranks=False)
+    return rtr
+
+def pca_transformer(factory, pca, verbose=False) -> TransformerBase:
+    """
+    Apply a PCA model to the queries and documents embeddings to compress it
+    input: qid, query_embs, docno, doc_embs
+    output: qid, query_embs, docno, doc_embs
+    """
+    import torch
+    def _apply_pca(df):
+        iter = range(len(df))
+        df["doc_embs"] = df["doc_embs"].map(lambda x : torch.from_numpy(pca.transform(x)).type(torch.float32))
+        df["query_embs"] = df["query_embs"].map(lambda x : torch.from_numpy(pca.transform(x)).type(torch.float32))
+        factory.args.dim = pca.n_components
+        return df
+    
+    return pt.apply.by_query(_apply_pca, add_ranks=False)
+
+def scorer(factory, verbose=False) -> TransformerBase:
+        """
+        Calculates the ColBERT max_sim operator using previous encodings of queries and documents
+        input: qid, query_embs, [query_weights], docno, doc_embs
+        output: ditto + score
+        """
+        import torch
+        colbert = factory.args.colbert
+        def _score_query(df):
+            weightsQ = None
+            Q = torch.cat([df.iloc[0].query_embs])
+            if "query_weights" in df.columns:
+                weightsQ = df.iloc[0].query_weights
+            else:
+                weightsQ = torch.ones(Q.shape[0])        
+            D = torch.zeros(len(df), factory.args.doc_maxlen, factory.args.dim)
+            iter = range(len(df))
+            if verbose:
+                iter = pt.tqdm(iter, total=len(df))
+            for i in iter:
+                doc_embs = df.iloc[i].doc_embs
+                doclen = doc_embs.shape[0]
+                D[i, 0:doclen, :] =  doc_embs
+            maxscoreQ = (Q @ D.permute(0, 2, 1)).max(2).values.cpu()
+            scores = (weightsQ*maxscoreQ).sum(1).cpu()
+            df["score"] = scores.tolist()
+            df = factory._add_docnos(df)
+            return df
+            
+        return pt.apply.by_query(_score_query)
+
+def blacklisted_tokens_transformer(factory, blacklist, verbose=False) -> TransformerBase:
+    """
+    Remove tokens and their embeddings from the document dataframe
+    input: qid, query_embs, docno, doc_embs, doc_toks
+    output: qid, query_embs, docno, doc_embs, doc_toks
+    
+    The blacklist parameters must contain a list of tokenids that should be removed
+    """
+    # factory.pruned_embeddings = 0
+    
+    import numpy as np
+    def _prune(row):
+        
+        import torch
+        
+        tokens = row['doc_toks']
+        final_mask = (tokens > -1)
+        
+        for element in blacklist:
+            element_mask = (tokens == element)
+            final_mask = final_mask & (~ element_mask)
+        
+        row_embs_size = row['doc_embs'].size()
+        
+        mask_1d = torch.cat((final_mask, torch.ones(row_embs_size[0] - final_mask.size()[0], dtype=torch.bool)))
+        mask_column = torch.unsqueeze(mask_1d, 1)
+        mask = mask_column.repeat(1, row_embs_size[1])
+        
+        row['doc_embs'] = row['doc_embs'][mask].reshape(mask_1d.count_nonzero(), row_embs_size[1])
+        row['doc_toks'] = tokens[final_mask]
+
+        pruned_embeddings = row_embs_size[0] - row['doc_embs'].size()[0]
+        if verbose: print("Embeddings removed:", pruned_embeddings)
+        # factory.pruned_embeddings += pruned_embeddings
+        return row
+
+    return pt.apply.generic(lambda df : df.apply(_prune, axis=1))
