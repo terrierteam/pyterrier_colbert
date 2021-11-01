ded5dcb31b7cbe0a92ea04acaecab84c5de37dc3
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-11-01 17:36:00 +0000

    fixed errors?

--- tests/test_text_scorer.py
+++ tests/test_text_scorer.py
@@ -1,11 +1,10 @@
 import unittest
 import tempfile
 from unittest.result import failfast
-
+import pyterrier as pt
 class TestIndexing(unittest.TestCase):
 
     def setUp(self):
-        import pyterrier as pt
         from pyterrier_colbert.ranking import ColBERTFactory
         if not pt.started():
             pt.init()
@@ -15,18 +14,18 @@ class TestIndexing(unittest.TestCase):
 
     def test_text_scorer(self):
         scorer = self.factory.text_scorer()
-        df = self.pt.new.ranked_documents([[1, 2]])
+        df = pt.new.ranked_documents([[1, 2]])
         df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
         df["query"] = ["chemical reactions", "chemical reactions"]
         rtr = scorer.transform(df)
         self.assertTrue("score" in rtr.columns)
 
     def test_text_scorer_with_qembs(self):
-        df = self.pt.new.ranked_documents([[1, 2]])
+        df = pt.new.ranked_documents([[1, 2]])
         df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
         df["query"] = ["chemical reactions", "chemical reactions"]
         scorer = self.factory.text_scorer()
-        qe_scorer =  self.pt.transformer.SourceTransformer(df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
+        qe_scorer =  pt.transformer.SourceTransformer(df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
         rtr = scorer.transform(df)  
         qe_rtr = qe_scorer.search(df["query"])
         self.assertTrue("score" in rtr.columns)
