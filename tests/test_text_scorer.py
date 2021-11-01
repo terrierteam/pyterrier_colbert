616d439531d7ce7c60b58c0b18fc0ac40eb03cbf
Hammad Khan <hammadkhann34@gmail.com>
2021-11-01 16:11:38 +0000

    remove redundant code from unit test

--- tests/test_text_scorer.py
+++ tests/test_text_scorer.py
@@ -1,36 +1,32 @@
 import unittest
 import tempfile
 from unittest.result import failfast
-import pyterrier as pt
 
 class TestIndexing(unittest.TestCase):
 
     def setUp(self):
         import pyterrier as pt
+        from pyterrier_colbert.ranking import ColBERTFactory
         if not pt.started():
             pt.init()
         self.test_dir = tempfile.mkdtemp()
+        checkpoint="http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
+        self.factory = ColBERTFactory(checkpoint, None, None, gpu=False)
 
     def test_text_scorer(self):
-        from pyterrier_colbert.ranking import ColBERTFactory
-        checkpoint="http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
-        factory = ColBERTFactory(checkpoint, None, None, gpu=False)
-        scorer = factory.text_scorer()
-        df = pt.new.ranked_documents([[1, 2]])
+        scorer = self.factory.text_scorer()
+        df = self.pt.new.ranked_documents([[1, 2]])
         df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
         df["query"] = ["chemical reactions", "chemical reactions"]
         rtr = scorer.transform(df)
         self.assertTrue("score" in rtr.columns)
 
     def test_text_scorer_with_qembs(self):
-        from pyterrier_colbert.ranking import ColBERTFactory
-        checkpoint="http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
-        factory = ColBERTFactory(checkpoint, None, None, gpu=False)
-        df = pt.new.ranked_documents([[1, 2]])
+        df = self.pt.new.ranked_documents([[1, 2]])
         df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
         df["query"] = ["chemical reactions", "chemical reactions"]
-        scorer = factory.text_scorer()
-        qe_scorer =  pt.transformer.SourceTransformer(df) >> factory.query_encoder() >> factory.text_scorer(query_encoded=True)  
+        scorer = self.factory.text_scorer()
+        qe_scorer =  self.pt.transformer.SourceTransformer(df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
         rtr = scorer.transform(df)  
         qe_rtr = qe_scorer.search(df["query"])
         self.assertTrue("score" in rtr.columns)
