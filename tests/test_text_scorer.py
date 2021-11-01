0bd86e92a25916e0231fd3c330e5be5d88c53932
Hammad Khan <hammadkhann34@gmail.com>
2021-11-01 22:24:57 +0000

    Update test_text_scorer.py

--- tests/test_text_scorer.py
+++ tests/test_text_scorer.py
@@ -12,22 +12,19 @@ class TestIndexing(unittest.TestCase):
         self.test_dir = tempfile.mkdtemp()
         checkpoint="http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
         self.factory = ColBERTFactory(checkpoint, None, None, gpu=False)
+        self.df = self.pt.new.ranked_documents([[1, 2]])
+        self.df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
+        self.df["query"] = ["chemical reactions", "chemical reactions"]
 
     def test_text_scorer(self):
         scorer = self.factory.text_scorer()
-        df = self.pt.new.ranked_documents([[1, 2]])
-        df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
-        df["query"] = ["chemical reactions", "chemical reactions"]
-        rtr = scorer.transform(df)
+        rtr = scorer.transform(self.df)
         self.assertTrue("score" in rtr.columns)
 
     def test_text_scorer_with_qembs(self):
-        df = self.pt.new.ranked_documents([[1, 2]])
-        df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
-        df["query"] = ["chemical reactions", "chemical reactions"]
         scorer = self.factory.text_scorer()
-        qe_scorer =  self.pt.transformer.SourceTransformer(df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
-        rtr = scorer.transform(df)  
-        qe_rtr = qe_scorer.search(df["query"])
+        qe_scorer =  self.pt.transformer.SourceTransformer(self.df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
+        rtr = scorer.transform(self.df)  
+        qe_rtr = qe_scorer.search(self.df["query"])
         self.assertTrue("score" in rtr.columns)
         self.assertTrue(rtr.equals(qe_rtr))
