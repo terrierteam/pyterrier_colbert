003831ebff6cc26def780c1093aacdd6cf1ce867
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-12-28 14:51:45 +0000

    use .stats to avoid confusing colbert

--- pyterrier_colbert/faiss_term_index.py
+++ pyterrier_colbert/faiss_term_index.py
@@ -81,7 +81,7 @@ class FaissNNTerm():
         self.tok = self.inference.query_tokenizer.tok
         vocab_size = self.tok.vocab_size
         if cf:
-            cfs_file = os.path.join(index_path, "cfs.pt")
+            cfs_file = os.path.join(index_path, "cfs.stats")
             if os.path.exists(cfs_file):
                 self.lookup = torch.load(cfs_file)
             else:
@@ -99,7 +99,7 @@ class FaissNNTerm():
         self.num_docs = len(self.doclens)
         self.end_offsets = np.cumsum(self.doclens)
         if df:
-            dfs_file = os.path.join(index_path, "dfs.pt")
+            dfs_file = os.path.join(index_path, "dfs.stats")
             if os.path.exists(dfs_file):
                 self.dfs = torch.load(dfs_file)
             else:
