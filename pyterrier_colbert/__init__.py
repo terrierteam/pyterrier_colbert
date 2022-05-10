5d7c45b8dca2b2e034c8bd4233ca2a73f8d4fcd6
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-05-10 15:39:45 +0100

    Transformers 4 support with transformer 3 checkpoint - addresses #40 (#41)

--- pyterrier_colbert/__init__.py
+++ pyterrier_colbert/__init__.py
@@ -27,7 +27,14 @@ def load_checkpoint(path, model, optimizer=None, do_print=True):
 
     checkpoint['model_state_dict'] = new_state_dict
 
-    model.load_state_dict(checkpoint['model_state_dict'])
+    # check for transformer version mismatch and patch address accordingly
+    import transformers
+    from packaging import version
+    strict = True
+    if version.parse(transformers.__version__).major >= 4 and 'bert.embeddings.position_ids' not in checkpoint['model_state_dict']:
+        strict = False
+    
+    model.load_state_dict(checkpoint['model_state_dict'], strict=strict)
 
     if optimizer:
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
