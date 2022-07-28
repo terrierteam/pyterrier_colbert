88efae00e926cba47a292190b485c86bc85008dd
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-07-28 17:41:50 +0100

    added unit test

--- tests/test_text_scorer.py
+++ tests/test_text_scorer.py
@@ -3,10 +3,9 @@ import tempfile
 from unittest.result import failfast
 import pyterrier as pt
 
-class TestIndexing(unittest.TestCase):
+class TestTextScoring(unittest.TestCase):
 
     def setUp(self):
-        from pyterrier_colbert.ranking import ColBERTFactory
         if not pt.started():
             pt.init()
         self.test_dir = tempfile.mkdtemp()
@@ -17,10 +16,27 @@ class TestIndexing(unittest.TestCase):
         self.df["text"] = [ "professor proton mixed the chemicals", "chemical brothers played that tune"]
         self.df["query"] = ["chemical reactions", "chemical reactions"]
 
-    def test_text_scorer(self):
-        scorer = self.factory.text_scorer()
-        rtr = scorer.transform(self.df)
-        self.assertTrue("score" in rtr.columns)
+    def test_text_encoder(self):
+        enc = self.factory.text_encoder()
+        rtr1 = enc.transform(self.df)
+        self.assertTrue("doc_embs" in rtr1.columns)
+        self.assertTrue("doc_toks" in rtr1.columns)
+        t1 = rtr1.iloc[0].doc_embs
+        self.assertEqual(128, t1.shape[1])
+        t2 = rtr1.iloc[1].doc_embs
+        self.assertEqual(128, t2.shape[1])
+
+    def test_text_scorer_cmp(self):
+        scorer1 = self.factory.text_scorer()
+        rtr1 = scorer1.transform(self.df)
+        self.assertTrue("score" in rtr1.columns)
+
+        scorer2 = self.factory.query_encoder() >> self.factory.text_encoder() >> self.factory.scorer(gpu=False)
+        rtr2 = scorer2.transform(self.df)
+        self.assertTrue("score" in rtr2.columns)
+
+        self.assertAlmostEqual(rtr1.iloc[0].score, rtr2.iloc[0].score)
+        self.assertAlmostEqual(rtr1.iloc[1].score, rtr2.iloc[1].score)
 
     def test_query_encoder(self):
         queries = pt.new.queries(["chemical reactions"])
@@ -31,7 +47,7 @@ class TestIndexing(unittest.TestCase):
 
     def test_text_scorer_with_qembs(self):
         scorer = self.factory.text_scorer()
-        qe_scorer =  pt.transformer.SourceTransformer(self.df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
+        qe_scorer = pt.transformer.SourceTransformer(self.df) >> self.factory.query_encoder() >> self.factory.text_scorer(query_encoded=True)  
         rtr = scorer.transform(self.df)  
         qe_rtr = qe_scorer.search(self.df["query"])
         self.assertTrue("score" in rtr.columns)
